812482650d8fe866a65351d201737a9e47d85542
vitthalcool <vitthal1309@gmail.com>
2021-07-30 17:36:30 +0530

    first commit

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 const awsServerlessExpress = require('aws-serverless-express')
 const app = require('./app')
 const server = awsServerlessExpress.createServer(app)
-
+console.log("Hi there");
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
